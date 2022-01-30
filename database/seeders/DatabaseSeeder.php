2bc9a607fb08583cc147e94027c0ec70c0cc4bac
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-01-30 18:13:51 +0400

    chore: add data in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,41 @@ class DatabaseSeeder extends Seeder
 	 */
 	public function run()
 	{
-		Movie::factory(3)->create();
-		Quote::factory(6)->create();
+		Movie::factory()->create([
+			'name' => ['en' => 'This is mine', 'ka' => 'ეს ჩემია'],
+		]);
+
+		Movie::factory()->create([
+			'name' => ['en' => 'Georgia', 'ka' => 'საქართველო'],
+		]);
+
+		Movie::factory()->create([
+			'name' => ['en' => 'Get out', 'ka' => 'გადი'],
+		]);
+
+		Quote::factory()->create([
+			'quote' => ['en' => 'There are so many chicken', 'ka' => 'აქ ძალიან ბევრი ქათამია'],
+		]);
+
+		Quote::factory()->create([
+			'quote' => ['en' => 'Italy', 'ka' => 'იტალია'],
+		]);
+
+		Quote::factory()->create([
+			'quote' => ['en' => 'Come here', 'ka' => 'მოდი აქ'],
+		]);
+
+		Quote::factory()->create([
+			'quote' => ['en' => 'There is no chickens', 'ka' => 'აქ არ არიან ქათმები'],
+		]);
+
+		Quote::factory()->create([
+			'quote' => ['en' => 'Paris', 'ka' => 'პარიზი'],
+		]);
+
+		Quote::factory()->create([
+			'quote' => ['en' => 'Now go away', 'ka' => 'ეხლა წადი აქედან'],
+		]);
 
 		User::factory()->create([
 			'name'     => 'luka',
