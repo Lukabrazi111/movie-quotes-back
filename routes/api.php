7d7af49c48c8b86377aea376806d7480af311832
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2025-11-10 01:44:25 +0100

    feat: create quote resources

--- routes/api.php
+++ routes/api.php
@@ -25,5 +25,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
     Route::put('/profile', [UserController::class, 'update'])->name('user.update');
+
+    // Movies
+
 });
